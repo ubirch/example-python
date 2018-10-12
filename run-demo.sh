b3d1fffc25b12989904a4a59d5c5f2f3c3f31f36
mrobakowski <mikolaj.robakowski@ubirch.com>
2018-10-12 16:12:07 +0200

    move the sourcecode to /src folder; improve the run-demo script

--- run-demo.sh
+++ run-demo.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
+cd $(dirname $0)
+
 python3 -m venv venv
 . ./venv/bin/activate
 
 pip --no-cache-dir install -r requirements.txt
 
 # redirects stderr, so make sure you have demo:stdout = true in the demo.ini file
-python demo.py 2>/dev/null
+python src/demo.py $1 2>/dev/null
