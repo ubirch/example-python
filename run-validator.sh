90c0688834f0c93b924e31abe614a2559b5198f8
mrobakowski <mikolaj.robakowski@ubirch.com>
2018-10-12 16:18:52 +0200

    improve run-validator script

--- run-validator.sh
+++ run-validator.sh
@@ -2,7 +2,7 @@
 
 export VALIDATOR_BACKEND_ADDRESS=https://api.ubirch.dev.ubirch.com/api/avatarService/v1/device/verify
 export VALIDATOR_ADDITIONAL_HEADERS="Authorization: $(
-    cat demo.ini | grep auth | cut -d' ' -f3,4
+    cat ${1:-demo.ini} | grep auth | cut -d' ' -f3,4
 )"
 
 docker run -dti -p8080:8080 -e VALIDATOR_ADDITIONAL_HEADERS -e VALIDATOR_BACKEND_ADDRESS ubirch/standalone-validator:1.0
